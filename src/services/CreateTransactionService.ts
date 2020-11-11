caf5aa92eb6554cb1e89945e6da8302a1180c70b
Marcio Sardou <marciosut1996@gmail.com>
2020-11-11 17:29:40 -0300

    finalizando alterações desafio gostack database-upload

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,9 +1,11 @@
 import { v4 as uuid } from 'uuid';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import AppError from '../errors/AppError';
 import TransactionRepository from '../repositories/TransactionsRepository'
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
 
 
 interface Request {
@@ -20,11 +22,35 @@ class CreateTransactionService {
   public async execute({title, type, value, category}: Request): Promise<Transaction> {
 
     const transactionRepository = getCustomRepository(TransactionRepository)
-    //validações a serem feitas
+    const categoryRepository = getRepository(Category)
+
+
+    const { total } = await transactionRepository.getBalance()
+
+    if ( type === 'outcome' && total < value) {
+      throw new AppError("You do not have enought balance")
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category
+      }
+    })
+
+    if(!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category
+      })
+
+      await categoryRepository.save(transactionCategory)
+    }
+
     const transaction = transactionRepository.create({
       title,
       type,
       value,
+      category: transactionCategory,
+
     })
 
     await transactionRepository.save(transaction)
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction';
+
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository)
+
+    const transaction = await transactionRepository.findOne(id)
+
+    if(!transaction) {
+      throw new AppError('Transaction does not exist')
+    }
+    await transactionRepository.remove(transaction)
   }
 }
 
